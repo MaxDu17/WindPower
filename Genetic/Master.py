4fdb7cd1b3af9bdc254938cd1827b74b8ee3624c
Max Du <maxdubusiness@gmail.com>
2018-11-25 19:31:27 -0500

    a

--- Genetic/Master.py
+++ Genetic/Master.py
@@ -1,7 +1,19 @@
 import sys
 import os
 import subprocess
+import random
+population_size = 10
+training_epoch = 20000
 
-a = subprocess.Popen(['/usr/bin/python3', './test.py', '1', '2', '3'])
+a =
 
-print("this is the return value" + a)
+genetic_matrix = []
+
+for i in range(population_size):
+    footprint = int(random.randint(1, 15))
+    learning_rate = random.randrange(0.00001, 0.1)
+    cell_dim = random.randint(1, 100)
+    hidden_dim = random.randint(1, 100)
+    genetic_matrix.append([footprint, learning_rate, cell_dim, hidden_dim])
+    subprocess.Popen(['/usr/bin/python3', './test.py', str(footprint), str(learning_rate, str(cell_dim, str(hidden_dim)))])
+
