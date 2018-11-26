586190a92cd988cd8f4cf40d17b44630306d4603
Max Du <maxdubusiness@gmail.com>
2018-11-25 19:42:28 -0500

    w

--- Genetic/Master.py
+++ Genetic/Master.py
@@ -8,7 +8,7 @@ genetic_matrix = []
 
 for i in range(population_size):
     footprint = int(random.randint(1, 15))
-    learning_rate = random.randrange(1, 20)*0.0005
+    learning_rate = round(random.randrange(1, 20)*0.0005, 6)
     cell_dim = random.randint(1, 100)
     hidden_dim = random.randint(1, 100)
     genetic_matrix.append([footprint, learning_rate, cell_dim, hidden_dim])
