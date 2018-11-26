61509dd8315b05784cccd9d9aba7e1b42bd9cc8f
Max Du <maxdubusiness@gmail.com>
2018-11-25 19:41:00 -0500

    w

--- Genetic/Master.py
+++ Genetic/Master.py
@@ -8,10 +8,11 @@ genetic_matrix = []
 
 for i in range(population_size):
     footprint = int(random.randint(1, 15))
-    learning_rate = random.randrange(1,10000)/100000
+    learning_rate = random.randrange(1, 20)*0.0005
     cell_dim = random.randint(1, 100)
     hidden_dim = random.randint(1, 100)
     genetic_matrix.append([footprint, learning_rate, cell_dim, hidden_dim])
     subprocess.Popen(['/usr/bin/python3', './test.py', str(footprint), str(learning_rate), str(cell_dim), str(hidden_dim)])
 
 print(genetic_matrix)
+print("-----------------------------------")
