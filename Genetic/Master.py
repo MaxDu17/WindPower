2aeacfe21dcaf60d004a027227e01dca012cccee
Max Du <maxdubusiness@gmail.com>
2018-12-23 21:52:53 -0500

    f

--- Genetic/Master.py
+++ Genetic/Master.py
@@ -25,7 +25,8 @@ exit_codes=[p.wait() for p in subprocess_array]
 
 for i in range(POPULATION_SIZE):
     data = open(str(i)+".csv", "r")
-    data_ = csv.reader(data, lineterminator = "\n")
+    data_ = list(csv.reader(data, lineterminator = "\n"))
+    data_ = [m[0] for m in k]
     print(data_)
     loss = float(data_[0])
     data_dict[i] = [genetic_matrix, loss]
