a6b0b998c6f5e4983e48e869095bf94cad8e013f
Max Du <maxdubusiness@gmail.com>
2018-12-29 01:45:04 -0500

    fixed dimenaionality agg

--- Genetic/Large_file.py
+++ Genetic/Large_file.py
@@ -184,8 +184,7 @@ def mutate_float(value):
 
 
 def cross_over(array_1, array_2):
-    print(array_1[0])
-    print(array_1)
+
     scratch_list = list()
     child_list = list()
     for i in range(POPULATION_SIZE - 2): #minus 2 b/c the parents will stay too
@@ -211,7 +210,7 @@ with tf.Session() as sess:
         genetic_matrix = [footprint, learning_rate, cell_dim, hidden_dim, TRAINING_EPOCHS, TEST_SIZE, i]
         results.append([genetic_matrix, graph(genetic_matrix, sess)])
     results.sort(key = sort_second)
-    results = results[0:2] #picking the top two hyperparameters
-    children = cross_over(results[0][0], results[1][0]) #this should g et the hyperparameters
+    results = results[0:2][0] #picking the top two hyperparameters
+    children = cross_over(results[0][0:5], results[1][0:5]) #this should g et the hyperparameters
     print(children)
 
