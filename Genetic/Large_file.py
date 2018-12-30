2659d9923ee267b3e4e68329d6da03000c43aceb
Max Du <maxdubusiness@gmail.com>
2018-12-30 00:54:54 -0500

    ok testing system

--- Genetic/Large_file.py
+++ Genetic/Large_file.py
@@ -4,7 +4,7 @@ import csv
 import tensorflow as tf
 from pipeline.dataset_maker import SetMaker
 import numpy as np
-POPULATION_SIZE = 2
+POPULATION_SIZE = 10
 TRAINING_EPOCHS = 500
 TEST_SIZE = 200
 ACTIVE_HYP = 4
@@ -184,11 +184,6 @@ def mutate_float(value):
 
 
 def cross_over(array_1, array_2):
-    array_1 = array_1[0:5]
-    #array_2 = array_2[0:5]
-    print(array_2)
-    print(array_1[0])
-    array_1_ = array_1[0:5]
     scratch_list = list()
     child_list = list()
     for i in range(POPULATION_SIZE - 2): #minus 2 b/c the parents will stay too
@@ -214,11 +209,8 @@ with tf.Session() as sess:
         genetic_matrix = [footprint, learning_rate, cell_dim, hidden_dim, TRAINING_EPOCHS, TEST_SIZE, i]
         results.append([genetic_matrix, graph(genetic_matrix, sess)])
     results.sort(key = sort_second)
-    results = [k[0] for k in results]
-    results = [k[0:5] for k in results]
-     #first gets the best 2, second gets only hyperparameter
-    print(results)
-    #results = [k[0:5] for k in results]
+    results = [k[0] for k in results] #removes the error. This is no longer needed
+    results = [k[0:5] for k in results] #removes the serial number. This is no longer needed
     children = cross_over(results[0], results[1]) #this should g et the hyperparameters
     print(children)
 
