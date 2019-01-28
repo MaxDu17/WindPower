030d195dc58b7ae3b40d66847f2c6697f0925c81
Max Du <maxdubusiness@gmail.com>
2019-01-27 23:44:58 -0500

    used the wrong version of save

--- Freeze_Programs/save_v4.py
+++ Freeze_Programs/save_v4.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 from tensorflow.python.tools import freeze_graph
-MODEL_NAME = 'LSTM_v2_frozen_CONTAINED'
+MODEL_NAME = 'LSTM_v2_genetic_frozen'
 
 # Freeze the graph
 
-input_graph_path = "2012/v2/GRAPHS_CONTAINED/graph.pbtxt"
-checkpoint_path = "2012/v2/models_CONTAINED/LSTMv2-60000"
+input_graph_path = "../Graphs_and_Results/lstm_v2_c_CSV_FED/GRAPHS/graph.pbtxt"
+checkpoint_path = "../Graphs_and_Results/lstm_v2_c_CSV_FED/models/V2Genetic-80000"
 input_saver_def_path = ""
 input_binary = False
 output_node_names = "prediction/output, forward_roll/pass_back_state"
 restore_op_name = "save/restore_all"
 filename_tensor_name = "save/Const:0"
-output_frozen_graph_name = '2012/v2/models_CONTAINED/'+MODEL_NAME+'.pb'
+output_frozen_graph_name = '../Graphs_and_Results/lstm_v2_c_CSV_FED/'+MODEL_NAME+'.pb'
 
 clear_devices = True
 
