6d8839d2365be1d2acf9b9dc8ce414254b750739
Max Du <maxdubusiness@gmail.com>
2019-01-28 01:28:57 -0500

    worked on some more stuff

--- WeatherDataRetrievalCode/local_saver/find_missing.py
+++ WeatherDataRetrievalCode/local_saver/find_missing.py
@@ -1,5 +1,4 @@
 import csv
-
 name = input("total set file name?\n")
 k = open("../../Training_Sets/" + name+ ".csv")
 rawset = list(csv.reader(k))
@@ -8,15 +7,10 @@ writer = csv.writer(m, lineterminator="\n")
 copy = list()
 print(len(rawset))
 copy.append(rawset[0])
-for j in range(1, len(rawset)-1):
 
-    print(j)
-    copy.append(rawset[j])
-    #if int(rawset[j][3]) != int((j-1)%24):
-    if int(rawset[j][3]) != (int(rawset[j+1][3]) - 1)%24:
-        print("true")
-        copy.append(["NO DATA FOUND"])
-        #print(rawset[j][0:4])
+MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+for j in range(1, len(rawset)-1):
+    hours = rawset[j][1] *
 
 print(len(copy))
 writer.writerows(copy)
