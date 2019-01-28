4c37f2fd67d993d1120192667effb8ed5ed04904
Max Du <maxdubusiness@gmail.com>
2019-01-28 00:55:36 -0500

    edited

--- WeatherDataRetrievalCode/local_saver/find_missing.py
+++ WeatherDataRetrievalCode/local_saver/find_missing.py
@@ -3,17 +3,20 @@ import csv
 name = input("total set file name?\n")
 k = open("../../Training_Sets/" + name+ ".csv")
 rawset = list(csv.reader(k))
-m = open("../../Training_Sets/MARKED_" + name + ".csv", "w")
+m = open("../../Training_Sets/INTERPOLATED_" + name + ".csv", "w")
 writer = csv.writer(m, lineterminator="\n")
-offset = 0
+copy = list()
 print(len(rawset))
-for j in range(1,366*24):
-    try:
-        if int(rawset[j][3]) != int((j-1-offset)%24):
-            rawset.insert(j, ["DATA NOT FOUND"])
-            #print(rawset[j][0:4])
-    except:
-        continue
+copy.append(rawset[0])
+for j in range(1, len(rawset)-1):
 
-print(len(rawset))
-writer.writerows(rawset)
+    print(j)
+    copy.append(rawset[j])
+    #if int(rawset[j][3]) != int((j-1)%24):
+    if int(rawset[j][3]) != (int(rawset[j+1][3]) - 1)%24:
+        print("true")
+        copy.append(["NO DATA FOUND"])
+        #print(rawset[j][0:4])
+
+print(len(copy))
+writer.writerows(copy)
--- diagnostics/step_plotter.py
+++ diagnostics/step_plotter.py
@@ -2,31 +2,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 upper_bound = 500
-lower_bound = 0
+lower_bound = 400
 step_length = 1
 
 FILE_NAME = "lstm_v2_c_CSV_FED"
 x = np.arange(0,step_length*(upper_bound-lower_bound),step_length)
 
-file_name_1 = "../Graphs_and_Results/Naive_case.csv"
 file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/GRAPHS/EVALUATE_TEST.csv"
 data2 = pd.read_csv(file_name_2)
-data1 = pd.read_csv(file_name_1)
 
-print(data2[["true_values"]])
-print(data1[["true_values"]])
-true_value = data1[["true_values"]]
-predicted_values1 = data1[["predicted_values"]]
+true_value = data2[["true_values"]]
 predicted_values2 = data2[["predicted_values"]]
 
 true = [k[0] for k in true_value.values]
 
-predict1 = [n[0] for n in predicted_values1.values]
 predict2 = [n[0] for n in predicted_values2.values]
+predict2_naive = np.roll(predict2, -1)
 
 
 plt.step(x, true[lower_bound:upper_bound], label='truth')
 #plt.step(x, predict1[lower_bound:upper_bound], label=('predict version ' + str(version_number_1)))
+plt.step(x, predict2_naive[lower_bound:upper_bound], label=('naive simulation'))
 plt.step(x, predict2[lower_bound:upper_bound], label=('predict version: ' + FILE_NAME))
 
 title = FILE_NAME + " and naive" + ". " +\
