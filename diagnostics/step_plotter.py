27baa359fd27eca5af0981d68195c9d8de075bb3
Max Du <maxdubusiness@gmail.com>
2019-01-27 23:59:28 -0500

    modified step plotter

--- diagnostics/step_plotter.py
+++ diagnostics/step_plotter.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 upper_bound = 500
 lower_bound = 400
 step_length = 1
-version_number_1 = 2
-version_number_2 = 9
+
+FILE_NAME = "lstm_v2_c_CSV_FED"
 x = np.arange(0,step_length*(upper_bound-lower_bound),step_length)
 
-file_name_1 = "2012/v" + str(version_number_1) + "/GRAPHS/EVALUATE_TEST.csv"
-file_name_2 = "2012/v" + str(version_number_2) + "/GRAPHS/v15/TEST.csv"
+file_name_1 = "../Graphs_and_Results/Naive_case.csv"
+file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/GRAPHS/EVALUATE_TEST.csv"
 
 data1 = pd.read_csv(file_name_1)
 data2 = pd.read_csv(file_name_2)
@@ -26,9 +26,9 @@ predict2 = [n[0] for n in predicted_values2.values]
 
 plt.step(x, true[lower_bound:upper_bound], label='truth')
 #plt.step(x, predict1[lower_bound:upper_bound], label=('predict version ' + str(version_number_1)))
-plt.step(x, predict2[lower_bound:upper_bound], label=('predict version '+ str(version_number_2)))
+plt.step(x, predict2[lower_bound:upper_bound], label=('predict version: ' + FILE_NAME))
 
-title = "version " + str(version_number_1) + " and " + str(version_number_2) + ". " +\
+title = FILE_NAME + " and naive" + ". " +\
         str(lower_bound)  + " to " + str(upper_bound) + ", step length " + str(step_length)
 
 plt.title(title)
