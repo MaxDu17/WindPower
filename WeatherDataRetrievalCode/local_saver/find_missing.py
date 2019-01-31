5134a62fa5caafab15c785364ad53b62012b91ea
Max Du <maxdubusiness@gmail.com>
2019-01-31 17:39:24 -0500

    fixed some issues

--- WeatherDataRetrievalCode/local_saver/find_missing.py
+++ WeatherDataRetrievalCode/local_saver/find_missing.py
@@ -1,24 +1,45 @@
 import csv
 import numpy as np
-name = input("total set file name?\n")
-k = open("../../Training_Sets/" + name+ ".csv")
-rawset = list(csv.reader(k))
-m = open("../../Training_Sets/TEST_" + name + ".csv", "w")
-writer = csv.writer(m, lineterminator="\n")
-copy = list()
-print(len(rawset))
-copy.append(rawset[0])
 
-MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-monthhours = [24*k for k in MONTHS]
-cumulativemonths = [sum(monthhours[0:k]) for k in range(12)]#this will get you the cumulative hours imparted by month
-print(cumulativemonths)
+def markup():
+    name = input("total set file name?\n")
+    k = open("../../Training_Sets/" + name+ ".csv")
+    rawset = list(csv.reader(k))
+    try:
+        m = open("../../Training_Sets/HOUR_" + name + ".csv", "w")
+    except:
+        print("please close the file!")
+    writer = csv.writer(m, lineterminator="\n")
+    copy = list()
+    print(len(rawset))
+    header = rawset[0]
+    header[0] = "hours"
+    del header[1:4]
+    copy.append(rawset[0])
 
-for j in range(1, len(rawset)-1):
+    MONTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    monthhours = [24*k for k in MONTHS]
+    cumulativemonths = [sum(monthhours[0:k]) for k in range(12)]#this will get you the cumulative hours imparted by month
+    print(cumulativemonths)
 
-    hours = int(rawset[j][3]) + int(rawset[j][2])*24 + cumulativemonths[int(rawset[j][1])-1]
+    for j in range(1, len(rawset)):
 
-    copy.append([hours, rawset[j][3:]])
+        hours = int(rawset[j][3]) + (int(rawset[j][2])-1)*24 + cumulativemonths[int(rawset[j][1])-1]
 
-print(len(copy))
-writer.writerows(copy)
+        carrier = rawset[j][4:19]
+        carrier.insert(0, hours)
+        copy.append(carrier)
+
+
+    print(len(copy))
+    writer.writerows(copy)
+    return copy
+
+def substitute(copy):
+
+
+def main():
+    pass
+
+
+
