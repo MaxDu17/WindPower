2fa633cdba04f8b3facc3b8f3f8edefefffd062b
Max Du <maxdubusiness@gmail.com>
2019-02-12 21:46:54 -0500

    d

--- WeatherDataRetrievalCode/local_saver/combineweatherandwind.py
+++ WeatherDataRetrievalCode/local_saver/combineweatherandwind.py
@@ -13,7 +13,6 @@ def main():
     weather_counter = 1
     carrier = wind[0]
     carrier.extend(weather[0][1:])
-    print(carrier)
     big_list = list()
     big_list.append(carrier)
 
@@ -21,9 +20,6 @@ def main():
     for i in range(1, len(wind)):
         c = wind[i]
         if int(wind[i][0]) == int(weather[weather_counter][0]) + 60:
-            #print(wind[i][0])
-            print(weather_counter)
-            #input()
             weather_counter += 1
         c.extend(weather[weather_counter][1:])
         big_list.append(c)
@@ -33,27 +29,33 @@ def main():
 
     final.writerows(big_list)
 
+    headers = big_list[0]
     transposed_list = np.array(big_list[1:]).T.tolist()
-    headers = transposed_list[0]
-    del transposed_list[0]
     print(headers)
-    normalized_list = transposed_list[0]
-    for parameter in transposed_list[1:]:
+
+    normalized_list = list()
+    normalized_list.append(transposed_list[0:1])
+    print(len(normalized_list))
+
+    for parameter in transposed_list[2:]:
         print(parameter[0])
         for k in range(0, len(parameter)): #this casts each from string to float
             try:
                 parameter[k] = float(parameter[k])
             except:
-                #print(k)
                 quit()
         maximum = max(parameter)
         minimum = min(parameter)
         range_ = maximum-minimum
         normalized_list.append([(x - minimum)/range_ for x in parameter])
 
+    print()
 
+    print(len(normalized_list))
     normalized_list = np.array(normalized_list).T.tolist()
 
+
+    normalized_list.insert(0, headers)
     normalized_ = open("../../Training_Sets/ALL_DATA_NORMALIZED.csv", "w")
     normalized = csv.writer(normalized_, lineterminator = "\n")
 
