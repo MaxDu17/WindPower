dc7f1cccd9352d8bb8a05607fd79bcee0dd8fd0f
Max Du <maxdubusiness@gmail.com>
2019-02-18 23:54:43 -0500

    a

--- Models/lstm_v6_c_CSV_FED.py
+++ Models/lstm_v6_c_CSV_FED.py
@@ -196,7 +196,7 @@ with tf.Session() as sess:
             RMS_loss = RMS_loss / hyp.Info.TEST_SIZE
             print("doing some rapid tests: this one had loss of " + str(RMS_loss))
             test_local_.close()
-            sm.reset_test_counter()
+
 
 ####################################VALIDATION#######################################
         if epoch % 2000 == 0 and epoch > 498: #this is the validation step
@@ -227,6 +227,7 @@ with tf.Session() as sess:
 #################################TESTING###############################################
     RMS_loss = 0.0
     next_state = np.zeros(shape=[2, 1, cell_dim])
+    sm.reset_test_counter()
     print(np.shape(next_state))
     for test in range(hyp.Info.TEST_SIZE):  # this will be replaced later
 
