202ba80a8aeb22f9345feac4beae94e41a940910
Max Du <maxdubusiness@gmail.com>
2019-02-20 00:20:28 -0500

    adapted some things

--- Freeze_Programs/save_v4.py
+++ Freeze_Programs/save_v4.py
@@ -2,22 +2,23 @@ import tensorflow as tf
 from tensorflow.python.tools import freeze_graph
 version = 6
 version_to_keep = 160000
-
+#commented things are for the RNN data
 MODEL_NAME = 'LSTM_v' + str(version) + '_genetic_frozen'
-MODEL_NAME = 'RNN_frozen'
+#MODEL_NAME = 'RNN_frozen'
 # Freeze the graph
 
-#input_graph_path = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/GRAPHS/graph.pbtxt'
-input_graph_path = '../Graphs_and_Results/RNN_class/GRAPHS/graph.pbtxt'
-#checkpoint_path = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/models/V' + str(version) + 'Genetic-' + str(version_to_keep)
-checkpoint_path = '../Graphs_and_Results/RNN_class/models/RNN-' + str(version_to_keep)
+input_graph_path = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/GRAPHS/graph.pbtxt'
+
+#input_graph_path = '../Graphs_and_Results/RNN_class/GRAPHS/graph.pbtxt'
+checkpoint_path = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/models/V' + str(version) + 'Genetic-' + str(version_to_keep)
+#checkpoint_path = '../Graphs_and_Results/RNN_class/models/RNN-' + str(version_to_keep)
 input_saver_def_path = ''
 input_binary = False
 output_node_names = 'prediction/output, forward_roll/pass_back_state'
 restore_op_name = 'save/restore_all'
 filename_tensor_name = 'save/Const:0'
-#output_frozen_graph_name = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/'+MODEL_NAME+'.pb'
-output_frozen_graph_name = '../Graphs_and_Results/RNN_class/'+MODEL_NAME+'.pb'
+output_frozen_graph_name = '../Graphs_and_Results/lstm_v' + str(version) + '_c_class/'+MODEL_NAME+'.pb'
+#output_frozen_graph_name = '../Graphs_and_Results/RNN_class/'+MODEL_NAME+'.pb'
 
 clear_devices = True
 
