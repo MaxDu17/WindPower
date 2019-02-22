1cb2d0ba1f269df6c5ea27287416338ce5a293a2
Max Du <maxdubusiness@gmail.com>
2019-02-22 01:11:30 -0500

    ok, did more stuff

--- diagnostics/step_plotter.py
+++ diagnostics/step_plotter.py
@@ -5,10 +5,11 @@ upper_bound = 500
 lower_bound = 400
 step_length = 1
 
-FILE_NAME = "gru_c_class"
+FILE_NAME = "RNN_class"
 x = np.arange(0,step_length*(upper_bound-lower_bound),step_length)
 
 file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/GRAPHS/EVALUATE_TEST.csv"
+#file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/data.csv"
 data2 = pd.read_csv(file_name_2)
 
 true_value = data2[["true_values"]]
@@ -24,7 +25,7 @@ plt.step(x, true[lower_bound:upper_bound], label='truth')
 
 plt.step(x, predict2[lower_bound:upper_bound], label=('predict version: ' + FILE_NAME))
 
-title = FILE_NAME + " and naive" + ". " +\
+title = FILE_NAME + " and truth" + ". " +\
         str(lower_bound)  + " to " + str(upper_bound) + ", step length " + str(step_length)
 
 plt.title(title)
