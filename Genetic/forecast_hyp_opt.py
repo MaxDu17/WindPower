13bb9a7aaf4cf97e4355054016996be630c5c090
Max Du <maxdubusiness@gmail.com>
2019-03-23 17:32:39 -0400

    v9 push

--- Genetic/forecast_hyp_opt.py
+++ Genetic/forecast_hyp_opt.py
@@ -8,8 +8,8 @@ import tensorflow as tf
 
 
 ############# CHANGE ME ###########
-from Models.gru_c_class_FORE import LSTM
-name = "gru_c_class_FORE"
+from Models.lstm_v9_c_class import LSTM
+name = "lstm_v9_c_class"
 ###################################
 
 
