735bac16716639df7d1e92ba042b0a0b3313d97d
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2020-12-24 15:10:39 -0500

    final commit

--- diagnostics/step_plotter.py
+++ diagnostics/step_plotter.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-upper_bound = 500
-lower_bound = 400
+upper_bound = 160
+lower_bound = 40
 step_length = 1
 
-FILE_NAME = "lstm_v2_c_class_FORE_AUTO"
-LABEL_NAME = "lstm v2 Forecast and Compression"
+FILE_NAME = "lstm_v1_c_class"
+LABEL_NAME = "Generic LSTM Prediction vs. Truth"
 x = np.arange(0,step_length*(upper_bound-lower_bound),step_length)
 
-file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/GRAPHS/EVALUATE_TEST__.csv"
+file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/GRAPHS/EVALUATE_TEST.csv"
 #file_name_2 = "../Graphs_and_Results/" + FILE_NAME + "/data.csv"
 data2 = pd.read_csv(file_name_2)
 
@@ -22,15 +22,15 @@ predict2 = [n[0] for n in predicted_values2.values]
 predict2_naive = np.roll(true, 1)
 
 
-plt.step(x, true[lower_bound:upper_bound], label='truth')
+plt.step(x, true[lower_bound:upper_bound], label='Truth')
 
-plt.step(x, predict2[lower_bound:upper_bound], label=('predict version: ' + LABEL_NAME))
+plt.step(x, predict2[lower_bound:upper_bound], label=('Prediction'))
 
-title = LABEL_NAME + " vs. Truth" + ". " +\
-        str(lower_bound)  + " to " + str(upper_bound) + ", step length " + str(step_length)
+#title = LABEL_NAME + " vs. Truth" + ". " +\
+ #       str(lower_bound)  + " to " + str(upper_bound) + ", step length " + str(step_length)
 
 
-plt.title(title)
+plt.title(LABEL_NAME)
 
 plt.legend()
 plt.grid()
